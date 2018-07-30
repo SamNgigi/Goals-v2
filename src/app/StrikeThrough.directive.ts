d5ea94989c1232f4c2e1ccc280cd85d18852bf4c
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-07-30 23:37:11 +0300

    StrikeThrough working.

--- src/app/StrikeThrough.directive.ts
+++ src/app/StrikeThrough.directive.ts
@@ -1,18 +1,50 @@
 // Note that we import the type ElementRef.
-import { Directive, ElementRef } from '@angular/core';
+// Note that we import the Input and HostListener decorators.
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appStrikeThrough]'
 })
 
 export class StrikeThroughDirective {
-
-  constructor(private element: ElementRef) {
-    /* 
+  /* 
       `element` refers to the constructor property that refers to the HTML element the directive will be hosted on.
 
-      We target the host element's style attribute and change the textDecoration to line-through.
+      
     */
-    this.element.nativeElement.style.textDecoration = "line-through";
+
+  constructor(private element: ElementRef) { }
+
+  /* 
+    The HostListener Annotation/Decorator is responsible for capturing events that occur on the host element.
+
+    We create 2 HostListener annotations. One that listens for single
+    clicks and another for double-clicks. 
+  */
+
+  @HostListener("click") onClicks() {
+    this.strikeThrough("line-through");
   }
+
+  @HostListener("dblclick") onDoubleClicks() {
+    this.strikeThrough("none");
+  }
+
+  /* 
+    We will create a the function strikeThrough that targets the
+    element reference and takes in a CSS style attribute as an
+    argument.
+  */
+
+  private strikeThrough(text_decoration_style: string) {
+
+    /* 
+      We target the host element's style attribute and change the
+      textDecoration to line-through when goal is complete or none when it is not.
+
+    */
+
+    this.element.nativeElement.style.textDecoration = text_decoration_style;
+  }
+
 }
